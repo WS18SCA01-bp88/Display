95586fb3f7776fcb53fc40a85a00ed165cdb7edd
BP88 <balmes.pavlov@gmail.com>
2018-12-15 14:22:15 -0500

    Initial commit

---
+++ Display/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Display
+//
+//  Created by Balmes Pavlov on 12/15/18.
+//  Copyright © 2018 Balmes Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
